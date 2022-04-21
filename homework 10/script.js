5b039003c925ec2180ab3c8ccc7fdec9147efff2
NastyaRyndia <nastya1992ryn@ukr.net>
2022-04-21 20:17:52 +0200

    script.js

---
+++ homework 10/script.js
@@ -0,0 +1,71 @@
+ 'use strict';
+
+//1.
+
+/*let a = 10;
+let b = 5;
+let remain = a % b;
+if (remain != 0) {
+	console.log (" Делится с остатком " + remain);
+} else {
+	console.log (" Делится, результат: " + (a / b));
+}
+*/
+
+
+//2.
+
+/*
+ function drawTriangle (){
+ 	for (let line = "+"; line.length < 7; line = line + "+") 
+ console.log(line);
+ }
+ drawTriangle ()
+*/
+
+
+//3.
+//2 способа "Выведите столбец нечетных чисел в промежутке от 0 до 100."
+
+/*
+for(let x = 0; x < 100; x++){
+	if(x % 2 === 0) {
+		continue;
+	}
+	console.log( x )
+}
+*/
+
+/*
+for(let x = 0; x < 100; x++){
+	if(x % 2 !==0)
+		
+	console.log( x )
+}
+*/
+
+//4.
+
+/*
+let n = 1000;
+let b = 0;
+while (n > 50) {
+	n = n / 2;
+	b++;
+}  
+console.log(n);
+console.log(b);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
