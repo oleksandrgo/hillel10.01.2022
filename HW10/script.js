69ed07413eb78b48742fff134cac0fd3dd34cb82
vladlena.deriabina <vladlenaderabina@gmail.com>
2022-04-24 22:51:37 +0300

    homwork

---
+++ HW10/script.js
@@ -0,0 +1,28 @@
+'use strict';
+/*let a = 15;
+let b = 8;
+let res = a % b;
+if (res !=0) {
+	console.log('Делится с остатком ' + res);
+} else {
+	console.log('Делится, результат деления: ' + (a / b));*/
+/*var c = 0,
+  d = 0;
+var max = 9;
+ var star = "*";
+while (c < max) {
+  star = "*";
+  for (d = 0; d < 1 * c; d++) star += "*";
+  console.log(star);
+  c++;
+} */
+ var f = 0;
+while(f<100){
+  f++;
+  if(f%2) console.log(f);
+}
+/* var n = 1000;
+ while ( n > 50) {
+ 	n = n / 2;
+ 	console.log ("n");
+ };*/
