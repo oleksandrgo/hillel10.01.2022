1c84a533ec030def3734db1565e82b89d94fb9f2
vladlena.deriabina <vladlenaderabina@gmail.com>
2022-04-28 01:10:56 +0300

    jsHW

--- HW10/script.js
+++ HW10/script.js
@@ -16,13 +16,27 @@ while (c < max) {
   console.log(star);
   c++;
 } */
- var f = 0;
+/* var f = 0;
 while(f<100){
   f++;
   if(f%2) console.log(f);
-}
+}*/
 /* var n = 1000;
  while ( n > 50) {
  	n = n / 2;
  	console.log ("n");
  };*/
+ var button = document.getElementById("btn1")
+ function firstEventFunction(){
+ 	var email = document.getElementById("1e").value;
+ 	var name = document.getElementById("1n").value;
+ 	var phone = document.getElementById("1p").value;
+ 	if (email.match(/(\w{6,12}@(gmail\.com)|(ukr\.net))/i)
+ 		&& phone.match(/^380+(50|63|66|67|68|91|92|93|96|97)\d{7}$/)
+ 		&& name.match(/^[a-zA-Z]{2,20}$/gm)){
+ 		alert("We will hear you");}
+ 	else {
+ 		alert ("We don't hear you");
+ 		}
+ 	}
+ 
