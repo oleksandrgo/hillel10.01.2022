e437ac128569d312d6285f9a76b9ed04dcb94d23
Anastasia <bykovanasta125@gmail.com>
2022-05-05 18:28:56 +0300

    HM10.1

--- HM10/script.js
+++ HM10/script.js
@@ -1,102 +1,57 @@
 "use strict";
-
-  
-  const form = document.querySelector("form");
-  const name = document.querySelector(".formName");
-  const mail= document.querySelector(".formMail");
-  const phone= document.querySelector(".formPhone");
-  const msg= document.querySelector(".formMsg");
-  const check = document.querySelector(".form-check-input");
-  let isSubmit = false;
-
-  const regExpName = /^[a-zA-Z0-9_-]{2,20}$/;
-const regExpEmeil = /[A-Z0-9._%+-]+(@gmail.com|ukr.net)[A-Z0-9-]+.+.[A-Z]{6,12}/;
-const regExpPhone = /^\+?\d{1,9}?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/;
-const regExpMsg = /^(?:(?![\s\n]+$)[\s\S])+$/;
-
-  const submit = () => {
-    alert("Мы услышим вас");
-
-    for (let elem of form.elements) {
-      if (
-        !elem.classList.contains("btn") &&
-        !elem.classList.contains("form-check-input")
-      ) {
-        elem.value = "" ;
-      }
-
-      if (elem.classList.contains("form-check-input")) {
-        elem.checked = false;
-      }
+let name ="Аnna"
+let re1 = /^[a-zA-Z0-9_-]{2,20}$/;
+ let button1=document.querySelector('#submit')
+submit.addEventListener("click",validateName)
+function validateName(){
+    if(!re1.test(name)){
+        alert('Мы услышим вас');
+        
+    }else{
+        alert('Мы вас не слышим');
+        
     }
-  };
+};
+let email = "qwerty@gmail.com "
+let re2 = /[a-zA-Z0-9._%+-]{6,12}.+@(gmail\.com | ukr\.net)/
+let button2=document.querySelector('#submit')
+submit.addEventListener("click",validateEmail)
+function validateEmail(){
+    if(!re2.test(email)){
+       alert ('Мы услышим вас');
+        
+    }else{
+      alert('Мы вас не слышим');
+        
+    }
+};
+let phone ="+380000000000"
+let re3 =/^\+380\.+50|63|66|67|68|91|92|93|97|96\d{7}$/
+let button3=document.querySelector('#submit')
+submit.addEventListener("click",validatePhone)
+function validatePhone(){
+    if(!re3.test(phone)){
+        alert('Мы услышим вас')
+        
+    }else{
+        alert('Мы вас не слышим');
+        
+    }
+};
 
-  const validate = (elem) => {
-    if (elem.name === "username") {
-      if (!regExpName.test(elem.value) && elem.value !== "" ) {
-        elem.nextElementSibling.textContent =
-          "Введите  имя пользователя";
-      } else {
-        elem.nextElementSibling.textContent == "";
-      }
+let msg = "Поиск номеров заказов на покупку, сделанных вашей компанией. Такие номера могут быть представлены в разном формате, например:"
+let re4 = /^(?:(?![\s\n]+$)[\s\S])+$/;
+let button4=document.querySelector('#submit')
+submit.addEventListener("click",validateMsg)
+function validateMsg(){
+    if(!re4.test(msg)){
+        alert('Мы услышим вас')
+        
+    }else{
+     alert('Мы вас не слышим');
+        
     }
+};
 
-    if (elem.email === "user_email") {
-      if (!regExpEmail.test(elem.value) && elem.value !== "") {
-        elem.nextElementSibling.textContent = "Введите корректный email";
-      } else {
-        elem.nextElementSibling.textContent = "";
-      }
-    }
 
-    if (elem.phone === "phone") {
-      if (!regExpPhone.test(elem.value) && elem.value !== "") {
-        elem.nextElementSibling.textContent = "Пожалуйста, заполните свой номер телефона +380 или 0";
-      } else {
-        elem.nextElementSibling.textContent = "";
-      }
-    }
 
-    if (elem.msg === "user_message") {
-      if (!regExpPhone.test(elem.value) && elem.value !== "") {
-        elem.nextElementSibling.textContent = "Введите сообщение";
-      } else {
-        elem.nextElementSibling.textContent = "";
-      }
-    }
-  };
-  for (let elem of form.elements) {
-    if (
-      !elem.classList.contains("btn") &&
-      !elem.classList.contains("form-check-input")
-    ) {
-      elem.addEventListener("blur", () => {
-        validate(elem);
-      });
-    }
-  }
-
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    for (let container of form.elements) {
-      if (
-        !container.classList.contains("btn") &&
-        !container.classList.contains("form-check-input")
-      ) {
-        if (container.value === ("")) {
-          container.nextElementSibling.textContent =("Данные не введены");
-          isSubmit = false;
-        } else {
-          isSubmit = true;
-        }
-      }
-    }
-
-    if (isSubmit) {
-      if (check.checked) {
-        submit();
-      } else {
-        alert("Подтвердите согласие с условиями");
-      }
-    }
-  });
