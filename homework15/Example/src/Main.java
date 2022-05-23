fe438f9cdcbb1ba14e01a93405b5c1df1152c7e3
fomanonbeliever <kooznetsovleoneed@gmail.com>
2022-05-23 15:09:24 +0300

    java1

---
+++ homework15/Example/src/Main.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Первая задача через "if"
+        /*System.out.print("Enter 1, 2 or 3 ");
+        Scanner inputFigure = new Scanner(System.in);
+        int i = inputFigure.nextInt();
+        if (i == 1) {
+            System.out.println("You got 1");
+        } else if (i == 2) {
+            System.out.println("You got 2");
+        } else if (i == 3) {
+            System.out.println("You got 3");
+        } else
+                System.out.println("You didn't enter 1,2 or 3");
+
+
+        }
+    }
+
+        //Первая задача через "switch"
+        System.out.println("Enter 1, 2 or 3");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+
+        switch (number){
+            case 1: System.out.println ("You got 1"); break;
+            case 2: System.out.println ("You got 2"); break;
+            case 3: System.out.println ("You got 3"); break;
+            default: System.out.println("You didn't enter 1,2 or 3");
+        }
+    }
+}
+
+
+        //Задача на 12345
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+        //Таблица умножения на 3
+        for (int i=1; i<=10; i++) {
+            System.out.println ("3*" + i + "=" + 3*i);
+        }
+    }
+}
+
+        //Среднее арифметическое от 1 до 100
+
+        double a = 0;
+        for (double i = 1; i <= 100; i++)
+            a += i;
+        System.out.println(a / 100);
+    }
+}*/
+
