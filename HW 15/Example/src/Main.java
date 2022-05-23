fd1eda74455dcf198647bcc219a6b38cc3e4a699
vladlena.deriabina <vladlenaderabina@gmail.com>
2022-05-23 03:20:23 +0300

    HW15

---
+++ HW 15/Example/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    /*public static void main(String [] args) {
+        System.out.println("Введите число 1, 2 или 3: ");
+        Scanner inputFigure = new Scanner(System.in);
+        int i = inputFigure.nextInt();
+        System.out.println(i);*/
+    /*    if (i==1) {
+            System.out.println("Вы ввели число 1");
+        }
+        else if (i==2) {
+            System.out.println("Вы ввели число 2");
+        }
+        else if (i==3) {
+            System.out.println("Вы ввели число 3");
+        }
+    }*/
+   /* public static void main(String [] args) {
+        System.out.println("Введите число 1, 2 или 3: ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        switch (number){
+            case 1: System.out.println ("Вы ввели число 1"); break;
+            case 2: System.out.println ("Вы ввели число 2"); break;
+            case 3: System.out.println ("Вы ввели число 3"); break;
+            default: System.out.println("Вы ввели неправильное число");
+        }
+    }*/
+   /* public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i+"");
+        }
+    }*/
+  /*  public static void main(String [] args) {
+        for (int i=1; i<=10; i++)
+            System.out.println("3*" + i + "=" + 3*i);
+    }*/
+    public static void main(String [] args) {
+        
+    }
+
+}
+
